c9b5aa313c3067a71c3e0c3431d65f1c7ddd461b
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-07-27 17:31:02 -0300

    Layout dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,62 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        {{ __('Dashboard') }}
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+    <div class="content-body">
+        <!-- Kick start -->
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Kick start your next project 🚀</h4>
+            </div>
+            <div class="card-body">
+                <div class="card-text">
+                    <p>
+                        Getting start with your project custom requirements using a ready template which is quite difficult and time
+                        taking process, Vuexy Admin provides useful features to kick start your project development with no efforts !
+                    </p>
+                    <ul>
+                        <li>
+                            Vuexy Admin provides you getting start pages with different layouts, use the layout as per your custom
+                            requirements and just change the branding, menu &amp; content.
+                        </li>
+                        <li>
+                            Every components in Vuexy Admin are decoupled, it means use use only components you actually need! Remove
+                            unnecessary and extra code easily just by excluding the path to specific SCSS, JS file.
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
+        <!--/ Kick start -->
+
+        <!-- Page layout -->
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">What is page layout?</h4>
+            </div>
+            <div class="card-body">
+                <div class="card-text">
+                    <p>
+                        Starter kit includes pages with different layouts, useful for your next project to start development process
+                        from scratch with no time.
+                    </p>
+                    <ul>
+                        <li>Each layout includes required only assets only.</li>
+                        <li>
+                            Select your choice of layout from starter kit, customize it with optional changes like colors and branding,
+                            add required dependency only.
+                        </li>
+                    </ul>
+                    <div class="alert alert-primary" role="alert">
+                        <div class="alert-body">
+                            <strong>Info:</strong> Please check the &nbsp;<a class="text-primary" href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/documentation/documentation-layouts.html#layout-collapsed-menu" target="_blank">Layout documentation</a>&nbsp; for more layout options i.e collapsed menu, without menu, empty & blank.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/ Page layout -->
+
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,48 +1,80 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="loading semi-dark-layout"  lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="semi-dark-layout" data-textdirection="ltr">
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+
+        <!-- BEGIN: Vendor CSS-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/vendors/vendors.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/semi-dark-layout.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/menu/vertical-menu.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/semi-dark-layout.css') }}">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
+
+            @include('layouts.header')
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+            <div class="app-content content ">
+                <div class="content-overlay"></div>
+                <div class="header-navbar-shadow"></div>
+                <div class="content-wrapper container-xxl p-0">
+                    <div class="content-header-left col-md-9 col-12 mb-2">
+                        <div class="row breadcrumbs-top">
+                          <div class="col-12">
+                            <h2 class="content-header-title float-start mb-0">
+                                {{ $header }}
+                            </h2>
+                            <div class="breadcrumb-wrapper">
+                              asdasd
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    {{ $slot }}
                 </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            </div>
         </div>
 
 
         <div class="sidenav-overlay"></div>
-    <div class="drag-target"></div>
+        <div class="drag-target"></div>
 
-    <!-- BEGIN: Footer-->
-    <footer class="footer footer-static footer-light">
-        <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">{{ env('APP_NAME', 'digitalfront') }} 2021</span><span class="float-md-end d-none d-md-block">Desenvolvido por digitalfront <i data-feather="heart"></i></span></p>
-    </footer>
-    <button class="btn btn-primary btn-icon scroll-top" type="button"><x-feathericon-arrow-up /></button>
-    <!-- END: Footer-->
+        <!-- BEGIN: Footer-->
+        <footer class="footer footer-static footer-light">
+            <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">{{ env('APP_NAME', 'digitalfront') }} 2021</span><span class="float-md-end d-none d-md-block">Desenvolvido por digitalfront <i data-feather="heart"></i></span></p>
+        </footer>
+        <button class="btn btn-primary btn-icon scroll-top" type="button"><x-feathericon-arrow-up /></button>
+        <!-- END: Footer-->
 
+        <script src="{{ asset('app-assets/js/vendors/vendors.min.js') }}"></script>
+        <script src="{{ asset('app-assets/js/app-menu.js') }}"></script>
+        <script src="{{ asset('app-assets/js/app.js') }}"></script>
+
+        <script>
+            $(window).on('load', function() {
+                if (feather) {
+                    feather.replace({
+                        width: 14,
+                        height: 14
+                    });
+                }
+            })
+        </script>
     </body>
 </html>
